8f139417f13c58837633f2f7e77a7e24847756a9
Flummi <flummi@f0ck.space>
2019-08-25 10:54:58 +0000

    typo lol

--- src/clients/irc/nick.mjs
+++ src/clients/irc/nick.mjs
@@ -2,7 +2,7 @@ export default bot => {
   bot._cmd.set("NICK", msg => { // nickchange
     let prefix = bot.parsePrefix(msg.prefix);
     if (bot.server.user.has(prefix.nick))
-      bot.server.user.del(prefix.nick);
+      bot.server.user.delete(prefix.nick);
     bot.whois(msg.params[0], true); // force
   });
 };
